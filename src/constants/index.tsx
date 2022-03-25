7bde46415c75acd127db8008a6e4c3f865705c8f
rBurgett <ryan@burgettdev.net>
2022-03-25 05:22:36 -0400

    Temporarily removed troublesome region endpoints

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,10 +1,10 @@
 export const AWS_REGIONS = [
-  'ap-east-1',
+  // 'ap-east-1',
   'ap-northeast-1',
   'ap-southeast-1',
   'eu-central-1',
   'eu-north-1',
-  'eu-south-1',
+  // 'eu-south-1',
   'eu-west-1',
   'us-east-1',
   'us-east-2',
